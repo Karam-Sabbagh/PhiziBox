ea6cc0fa1d423d38043807cc3e4d05f0210772cf
Karam Sabbagh <83864350+Karam-Sabbagh@users.noreply.github.com>
2022-01-19 20:54:26 +0200

    Update Pygame_gui_setup.py

--- Engine/Gui/Pygame_gui_setup.py
+++ Engine/Gui/Pygame_gui_setup.py
@@ -56,3 +56,73 @@ class Gui:
                      "top": "top",
                      "bottom": "bottom"}
         )
+
+    
+    def make_choose_object_window(self):
+        """
+        makes a window to let the user select the object
+        """
+        if not self.ui_window_exist:
+            self.ui_window = self.pygame_gui.elements.ui_window.UIWindow(self.pygame.Rect(self.screen_width/3, self.screen_height/3, 400, 300),
+                                                                         window_display_title="Choose Object",
+                                                                         resizable=False,
+                                                                         manager=self.ui_manager,
+                                                                         object_id="#choose_object_window")
+            self.ui_window_exist = True
+
+            self.pygame_gui.elements.UIButton(relative_rect=self.pygame.Rect(0, 0, 90, 70),
+                                              text="coming...",
+                                              manager=self.ui_manager,
+                                              container=self.ui_window,
+                                              object_id="#choose_rect_button"
+                                              )
+
+    def check_events(self, event):
+        """
+        :param event: it's from pygame.event.get()
+        this function is for checking pygame_gui things
+        and it's used for like checking button press or ui window close
+        """
+        if event.type == self.pygame_gui.UI_WINDOW_CLOSE:
+            if event.ui_object_id == "#choose_object_window":
+                self.ui_window_exist = False
+
+        self.ui_manager.process_events(event)
+
+    def check_each_selection_list_item(self):
+        """
+        it just checks the click of each button and item in the selection list that's made with self.make_selection_list
+        """
+        i = 1
+        for each_item in self.selection_list.item_list:
+            button_element = each_item["button_element"]
+            # sometimes the button is slided up and not really shown so pygame_gui remove this button when it's not seen able and makes it None
+            if button_element != None:
+                """
+                button element have
+                held = False
+                pressed = False
+                is_selected = False
+                object_id
+                this can be used
+                """
+                if button_element.pressed and button_element.is_selected:
+                    print("button", i, "pressed")
+                    self.make_choose_object_window()
+
+                button_rect = button_element.relative_rect
+                # the pressed button x position
+                button_pos_x = button_rect.x
+                # the pressed button y position
+                button_pos_y = button_rect.y
+            i += 1
+
+    def draw(self, time_delta):
+        """
+        draws everything about pygame_gui and also does somethings for them
+        :param time_delta: it is like time_delta = clock.tick(FPS) so it's just FPS of the screen at the second
+        """
+        self.check_each_selection_list_item()
+
+        self.ui_manager.update(time_delta)
+        self.ui_manager.draw_ui(self.pygame_ui_screen)
